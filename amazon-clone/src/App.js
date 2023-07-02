c5d6a2a329a8c257365224cf6db297bb96d571e6
Sandeep Jetwani <sandeepjetwani@gmail.com>
2023-07-02 16:22:57 +0530

    update

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -7,6 +7,14 @@ import Checkout from './Checkout.js'
 import Login from './Login.js'
 import {auth} from "./firebase"
 import { useStateValue } from './StateProvider';
+import Payment from './Payment.js';
+import {loadStripe} from "@stripe/stripe-js"
+import {Elements} from "@stripe/react-stripe-js"
+import Orders from './Orders.js'
+
+
+const promise = loadStripe("pk_test_51LTiEoSBTWWZUCk5I5q9xhOHbUff2ciWI7a2k2V2xXUK4y3gRduU9QRgalSrOde4JVLo4787NaXWxSwrU3rSICaR00o4vo8YO5")
+
 
 function App() {
 
@@ -44,6 +52,14 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/" element={<Home/>} />
+        <Route path="/payment" 
+               element={
+                <Elements stripe={promise} >
+                  <Payment /> 
+                </Elements>
+                }
+        />
+        <Route path= "/orders" element={<Orders />} />
         
       </Routes>
     </div>
